441be4491fcff42b1863d7dc24f8f68d527dddcf
elespantosoterry <jose.zm.13@gmail.com>
2020-01-16 20:52:25 -0500

    1.2

--- SistemaRestaurant/SistemaRestaurant/Form1.cs
+++ SistemaRestaurant/SistemaRestaurant/Form1.cs
@@ -6,7 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;  //BASE DE DATOS
+using System.Windows.Forms;
+using System.Data.SqlClient; //BASE DE DATOS
 
 namespace SistemaRestaurant
 {
@@ -15,13 +16,24 @@ namespace SistemaRestaurant
         public Form1()
         {
             InitializeComponent();
+            BD.cnn.Open();
         }
 
 
         // TEST - Jose Z
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("asdas");
+            SqlCommand command;
+            String sql, Output = "";
+            SqlDataReader dataReader;
+            sql = "select * from test";
+            command = new SqlCommand(sql, BD.cnn);
+            dataReader = command.ExecuteReader();
+            while (dataReader.Read())
+            {
+                Output = Output + dataReader.GetValue(0) + "\n";
+            }
+            MessageBox.Show(Output);
         }
         // -------------------
     }
