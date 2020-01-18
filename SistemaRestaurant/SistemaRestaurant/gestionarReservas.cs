18c1179c2276fd58c5a1e1bf1daae0b0b171f54f
joseCornejoLupa <jose.cornejo.lupa@gmail.com>
2020-01-18 15:17:13 -0500

    1.5.9

---
+++ SistemaRestaurant/SistemaRestaurant/gestionarReservas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//1.5.4 -> creacion de form gestionarReserva  LUPA
+
+namespace SistemaRestaurant
+{
+    public partial class gestionarReservas : MaterialSkin.Controls.MaterialForm
+    {
+        public gestionarReservas()
+        {
+            InitializeComponent();
+        }
+
+        private void gestionarReservas_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
