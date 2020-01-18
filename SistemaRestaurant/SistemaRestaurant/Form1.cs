f5366a77ec51c07e6e2ea9bace069a0dda9fa8b4
elespantosoterry <jose.zm.13@gmail.com>
2020-01-18 10:57:44 -0500

    1.5

--- SistemaRestaurant/SistemaRestaurant/Form1.cs
+++ SistemaRestaurant/SistemaRestaurant/Form1.cs
@@ -9,9 +9,14 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient; //BASE DE DATOS
 
+/*
 
-// 1.0 inciial - Jose Z
-// 1.1 prueba de base de datos - Jose Z
+1.0 inciial - Jose Z
+1.1 prueba de base de datos - Jose Z
+1.2 prueba de base de datos 2 - Jose Z
+1.3 prueba de base de datos 3 - Jose Z
+1.4 prueba de base de datos con datos artificiales - Jose Z
+*/
 
 namespace SistemaRestaurant
 {
---
+++ SistemaRestaurant/SistemaRestaurant/login.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaRestaurant
+{
+    public partial class login : MaterialSkin.Controls.MaterialForm
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
