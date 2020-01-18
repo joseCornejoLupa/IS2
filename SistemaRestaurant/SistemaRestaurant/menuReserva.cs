38ea83210111318eae7275d310ae51cfbafad1a4
joseCornejoLupa <jose.cornejo.lupa@gmail.com>
2020-01-18 12:07:55 -0500

    1.5.4

---
+++ SistemaRestaurant/SistemaRestaurant/menuReserva.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaRestaurant
+{
+    public partial class menuReserva : MaterialSkin.Controls.MaterialForm
+    {
+        public menuReserva()
+        {
+            InitializeComponent();
+        }
+
+        private void menuReserva_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
