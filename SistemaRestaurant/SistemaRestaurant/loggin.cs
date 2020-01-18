01d614555d9daf516512311afd297d0dba5c1d71
elespantosoterry <jose.zm.13@gmail.com>
2020-01-18 14:48:57 -0500

    1.5.6

--- SistemaRestaurant/SistemaRestaurant/loggin.cs
+++ SistemaRestaurant/SistemaRestaurant/loggin.cs
@@ -8,6 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+/*
+ * 1.5.1 Creacion de formulario login - Jose Z
+ * 1.5.6 Diseño de ventana login y menu de Chef y Bartender - Jose Z
+ */
+
 namespace SistemaRestaurant
 {
     public partial class login : MaterialSkin.Controls.MaterialForm
@@ -20,6 +26,31 @@ namespace SistemaRestaurant
         private void loggin_Load(object sender, EventArgs e)
         {
            
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+          
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialFlatButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
